90d138589d5b25234830ca80e11fdcc2f491ce62
joruso <148150523+joruso@users.noreply.github.com>
2024-04-24 12:34:34 +0200

    climacontrol.h

--- main/climacontrol.h
+++ main/climacontrol.h
@@ -1,16 +1,18 @@
 /*
- * climacontrol.h
+ * climate_control.h
  *
  *  Created on: 29 ene 2024
  *      Author: Joruso
  */
 
-#ifndef MAIN_CLIMACONTROL_H_
-#define MAIN_CLIMACONTROL_H_
+#ifndef MAIN_CLIMATECONTROL_H_
+#define MAIN_CLIMATECONTROL_H_
+
+#include "esp_log.h"
 
 
-
-void clima_init ();
-void config_hora_();
+void climate_init();
+void config_light(uint8_t hour_on, uint8_t min_on,
+                  uint8_t hour_off, uint8_t min_off);
 
 #endif /* MAIN_CLIMACONTROL_H_ */
